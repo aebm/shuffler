a2582b3352582034af1b8dff99d4ac39a15d9b54
Alejandro Brito Monedero <abrito@alea-soluciones.com>
2015-02-23 11:28:32 +0100

    Fix script to pass pep8

--- shuffler.py
+++ shuffler.py
@@ -4,16 +4,19 @@ import argparse
 import random
 import sys
 
-DESCRIPTION = '''Shuffle the arguments received, if called without arguments 
+DESCRIPTION = '''Shuffle the arguments received, if called without arguments
                  the lines read from stdin will be shuffled and printed to
                  stdout'''
+
 
 def get_list():
     return sys.stdin.readlines()
 
+
 def print_list(list_):
     for elem in list_:
         print(elem.rstrip())
+
 
 def main():
     parser = argparse.ArgumentParser(description=DESCRIPTION)
@@ -23,6 +26,7 @@ def main():
         list_ = get_list()
     r.shuffle(list_)
     print_list(list_)
+
 
 if __name__ == '__main__':
     main()
